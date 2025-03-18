90d93ec3de6f5b49d4d5d51280842f2872628988
Jazzy-girl <rsw5713@g.rit.edu>
2025-03-17 20:49:06 -0400

    added thermistor msg 2!

--- Experimentation/ReadingData/DbcHex.py
+++ Experimentation/ReadingData/DbcHex.py
@@ -1,8 +1,8 @@
+from queue import Full
 import cantools, can
 import cantools.database
 
-# Load the DBC file
-db = cantools.database.load_file("Experimentation/DBC Data/start2.dbc")
+
 
 # Example raw CAN frame
 """
@@ -20,16 +20,50 @@ VALID_IDs = [
     #'03B',
     #'3CB',
     #'6B2',
-    '080'
+    #'080',
+    '081'
 ]
 
 MESSAGES = set()
 INVALID_IDs = set()
 
+FIELDS = set()
+
+information = dict()
+information['CRCChecksum'] = None
+information['PackCurrent'] = None
+information['PackDCL'] = None
+information['PackInstVoltage'] = None
+information['PackAmphours'] = None
+information['SimulatedSOC'] = None
+information['PackOpenVolt'] = None
+information['PackCCL'] = None
+information['ThermistorValue'] = None
+information['PackRes'] = None
+information['FaultPresent'] = None
+information['HighTemp'] = None
+information['CustomFlag'] = None # ?? tbis is not the right implementation. CustomFlag currently contains multiple pieces of information.
+                                # consider you've implemented the CustomFlag .dbc w/out interpreting data that used the CustomFlag correctly.
+                                # '28' as a value is vestigial. Test with new data from Rachael.
+information['LowTemp'] = None
+information['HighLowIndicator'] = None
+information['PackSOC'] = None
+
+# Need to update each specified field whenever it is updated.
+# Have a Dict of fields & values. The UI accesses and displays the field : value s of the dict.
+# Recognize which field and value is being returned, update the specified value.
+
+# How does Custom Flag work?? right now it just returns 28... 
+# isn't it supposed to return discrete and separate pieces of information in bits?
+
 # Needed Ids:
 # 0x08, 0x81, 0x82
 
-filename = "Experimentation/ReadingData/TestData/CANData1/2025-03-01-14-24-30.txt"
+# Load the DBC file
+db = cantools.database.load_file("Experimentation/DBC Data/FaultsWithThermistorData.dbc")
+
+# Load data fike
+filename = "Experimentation/ReadingData/TestData/CANData1/3.17_SMALL.txt"
 with open(filename, 'r') as file:
     for line in file:
         # Process each line
@@ -43,11 +77,22 @@ with open(filename, 'r') as file:
         if(hex_string in VALID_IDs):
             data = bytes.fromhex(line[5:])
             message = db.decode_message(id, data)
+            #print(f"Thermistor Temp: {message['ThermistorValue']} degrees Celsius")
+            """
+            for key in message.keys():
+                information[key] = message[key]
+                if(information['HighLowIndicator'] == 5):
+                    information['HighLowIndicator'] = 'Low'
+                else:
+                    information['HighLowIndicator'] = 'High'
+            """
             print(message)
+            #print(information)
         else:
            # print(f"Unknown CAN ID {hex(id)}.")
             INVALID_IDs.add(id)
     print(f"Unknown IDs: {INVALID_IDs}")
+    print(f"Fields: {FIELDS}")
         
 """
 
@@ -60,7 +105,7 @@ for can_id, data_hex in messages:
         print(f"Unknown CAN ID {hex(can_id)}, check DBC file.")
 """
 
-
+"""
 messages = [
     (0x03B, "FFFE00003DCE09"),
     (0x3CB, "061600A0CE0C"),
@@ -70,6 +115,8 @@ messages = [
 print("Messages in DBC file:")
 for message in db.messages:
     print(f"ID: {hex(message.frame_id)} - Name: {message.name}")
+
+"""
 
 
 """
