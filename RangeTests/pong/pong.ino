06cdae87761ed7c46f5c20d70e0d4a5fc8018892
nb-defibrillator <bokurimono@gmail.com>
2026-01-28 23:19:50 -0500

    Fixed pin location and shortend printed message

--- RangeTests/pong/pong.ino
+++ RangeTests/pong/pong.ino
@@ -1,7 +1,7 @@
 // change this if we wire it differently
 #define RFM95_CS   8
 #define RFM95_RST  4
-#define RFM95_INT  3
+#define RFM95_INT  2
 
 #include "common.h"
 
@@ -14,20 +14,27 @@ void setup() {
 }
 void loop() {
     uint8_t len = RH_RF95_MAX_MESSAGE_LEN;
-    if (rf.available() && rf.recv((uint8_t*)buf, &len)) {
-        if (len == MESSAGE_LEN && !memcmp(buf, BASE_PING, PREFIX_LEN)) {
-            buf[1] = 'O'; // change the PING to PONG
+    if (rf.available()) {
+        if (rf.recv((uint8_t*)buf, &len)) {
+            if (len == MESSAGE_LEN && !memcmp(buf, BASE_PING, PREFIX_LEN)) {
+                buf[4] = 'O'; // change the PING to PONG
 
-            long sendStart = micros();
-            rf.send((uint8_t*)buf, MESSAGE_LEN);
-            rf.waitPacketSent();
-            long sendEnd = micros();
+                long sendStart = micros();
+                rf.send((uint8_t*)buf, MESSAGE_LEN);
+                rf.waitPacketSent();
+                long sendEnd = micros();
 
-            Serial.print("Sent in ");
-            Serial.print(sendEnd - sendStart);
-            Serial.println("us");
+                Serial.print("Sent in ");
+                Serial.print(sendEnd - sendStart);
+                Serial.println("us");
+            } else {
+                printUnexpected(len);
+            }
         } else {
-            printUnexpected(len);
+            Serial.println("Recv failed");
         }
+    } else {
+        // Serial.println("No available packet");
+        //delay(10);
     }
 }
