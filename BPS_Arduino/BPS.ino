0fb54ce8c81bff963bb0224f4fc7ea8f5e0ffffb
Jazzy-girl <rsw5713@g.rit.edu>
2026-01-31 13:12:36 -0500

    started CAN interfacing

--- BPS_Arduino/BPS.ino
+++ BPS_Arduino/BPS.ino
@@ -1,1 +1,24 @@
-//Does the BPS Arduino stuff :3
+/**
+ * Author: Ryanne Wilson
+ * BPS Arduino for Props.
+ * 
+ */
+
+ // INCLUDE
+ #include <Arduino_CAN.h>
+ void setup(){
+    Serial.begin(115200)
+    while(!Serial);
+
+    if(!CAN.begin(CanBitRate::BR_250k)){
+      Serial.println("CAN.begin(...) failed.");
+      for(;;) {}
+   }
+ }
+
+ void loop(){
+   if(CAN.available()){
+      CanMsg const msg = CAN.read();
+      Serial.println(msg);
+   }
+ }
