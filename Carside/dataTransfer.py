6450f7b16aef2c965f56ebfa951c9b2132c84720
Jazzy-girl <rsw5713@g.rit.edu>
2026-01-31 11:52:29 -0500

    added more documentation and some setup

--- Carside/dataTransfer.py
+++ Carside/dataTransfer.py
@@ -8,10 +8,9 @@ Setup:
 
 pip3 install adafruit-circuitpython-rfm9x
 
-and maybe also:
-
 pip3 install adafruit-circuitpython-lis3dh
 
+pip3 install adafruit_ssd1306 ???
 """
 
 """
@@ -34,4 +33,32 @@ Speed: 40-46
     - 40-44: GPS speed (32-bit float km/h)
     - 44-46: Motor speed (16-bit unsigned int, pulses/second)
 """
+
+# System libraries
+import time
+# Blinka Libraries
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+# import SSD1306 module (for display-- not necessary)
+# import adafruit_ssd1306
+# import RFM9x
+import adafruit_rfm9x
+
+# Configure LoRa Radio
+
+# Pin for Chip Select; enables device for comms
+CS_PIN = board.CE1 
+CS = DigitalInOut(CS_PIN) # Chip Select accessor
+
+# Pin for Reset
+RESET_PIN = board.D25
+RESET = DigitalInOut(RESET_PIN) # Reset accessor
+MOSI = board.MOSI # Main Output, Secondary Input; data sent from board to transceiver
+MISO = board.MISO # Main Input, Secondary Output; data sent from transceiver to board
+FREQUENCY = 915.0
+spi = busio.SPI(board.SCK, MOSI=MOSI, MISO=MISO)
+rfm9x = adafruit_rfm9x(spi, CS, RESET, FREQUENCY)
+rfm9x.tx_power = 23
+prev_packet = None
 
