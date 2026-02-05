c205d4eb4ebb03034b746ba366ca923b22d57b68
Matthew Cornell <mcornell400708@gmail.com>
2026-02-04 20:24:13 -0500

    Switch to using the MCP2515 for CAN

--- DataCollection/collection.py
+++ DataCollection/collection.py
@@ -10,25 +10,20 @@ import digitalio
 import adafruit_rfm9x
 import adafruit_gps
 import adafruit_mcp2515
-from adafruit_mcp2515.canio import Message, RemoteTransmissionRequest
+from adafruit_mcp2515.canio import Message
 import adafruit_mcp3xxx.mcp3008 as mcp
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 sys.path.append(__file__ + "/..")
 import packet
 
-# BMS / CANbus
-import cantools, can, serial
-
 uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
 spi0 = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 spi1 = busio.SPI(board.SCK_1, MOSI=board.MOSI_1, MISO=board.MISO_1)
 
 adc_cs = digitalio.DigitalInOut(board.D24)
-can_cs = digitalio.DigitalInOut(board.D25)
 
 gps = adafruit_gps.GPS(uart, debug=False)
-can = adafruit_mcp2515.MCP2515(spi1, can_cs, loopback=False, silent=False)
 adc = mcp.MCP3008(spi1, adc_cs)
 
 thermistor = AnalogIn(adc, mcp.P0)
@@ -65,6 +60,7 @@ class SenderWorker(threading.Thread):
         self.lora_cs = digitalio.DigitalInOut(board.D13)
         FREQUENCY = 915
         self.lora = adafruit_rfm9x.RFM9x(spi0, self.lora_cs, self.lora_rst, FREQUENCY)
+        self.start()
     def run(self):
         while True:
             if self.to_send is None:
@@ -78,12 +74,15 @@ class CanBusWorker(threading.Thread):
     def __init__(self):
         self.daemon = True
         self.message = bytearray(13)
-        self.listener = can.listen(timeout=0.1)
+        self.can_cs = digitalio.DigitalInOut(board.D25)
+        self.can = adafruit_mcp2515.MCP2515(spi1, self.can_cs, loopback=False, silent=False)
+        self.start()
     def run(self):
-        while True:
-            msg = self.listener.receive()
-            if msg is not None:
-                self.message = msg
+        with self.can.listen() as listener:
+            while True:
+                msg = self.can.receive()
+                if isinstance(msg, Message):
+                    self.message = msg.data
 
 speed = SpeedWorker()
 sender = SenderWorker()
@@ -95,11 +94,10 @@ while True:
     lon = gps.longitude if gps.has_fix else 0
     lat = gps.latitude if gps.has_fix else 0
     temp = thermistor.value
-    curr, volt, soc, health, amph, hitemp, lotemp, avgtemp, hstemp, faults = get_can()
-    gpsSpeed = gps.speed_kmh if gps.has_fix and gps.speed_kmh is not None else 0
-    motorSpeed = speed.pulses()
+    gps_speed = gps.speed_kmh if gps.has_fix and gps.speed_kmh is not None else 0
+    motor_speed = speed.pulses()
     checksum = 0
-    pack = packet.RawPacket.without_bms(timestamp, lon, lat, temp, gpsSpeed, motorSpeed)
+    pack = packet.RawPacket.without_bms(timestamp, lon, lat, temp, gps_speed, motor_speed)
     data = pack.pack_bytes(False)
     data[26:39] = canbus.message
     packet.write_checksum(data)
